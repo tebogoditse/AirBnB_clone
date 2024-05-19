9e13b85c4a91a0e58f96e9e1a050c64f691b1520
tebogoditse <tebogoditse2@gmail.com>
2024-05-20 01:38:30 +0200

    all

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,17 @@ from datetime import datetime
 class BaseModel():
     """Base model class definition"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        dt_string = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, dt_string))
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         class_name = self.__class__.__name__
