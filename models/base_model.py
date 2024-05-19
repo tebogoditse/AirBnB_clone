2d89c00fdce2a9c06e2d19b676444c66ae35c329
tebogoditse <tebogoditse2@gmail.com>
2024-05-19 03:43:27 +0200

    all

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+"""Base Model Class"""
+
 import uuid
+from datetime import datetime
+
+
+class BaseModel():
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        updated_at = datetime.utcnow()
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+
+        return inst_dict
