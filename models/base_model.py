e855a81cff20262b6a1bdff0c544973a400d2171
tebogoditse <tebogoditse2@gmail.com>
2024-05-19 04:57:08 +0200

    all

--- models/base_model.py
+++ models/base_model.py
@@ -6,18 +6,19 @@ from datetime import datetime
 
 
 class BaseModel():
+    """Base model class definition"""
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        updated_at = datetime.now()
 
     def __str__(self):
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         inst_dict = self.__dict__.copy()
