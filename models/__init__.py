d0297eba189a570e928d6ea9c4896563406760ad
tebogoditse <tebogoditse2@gmail.com>
2024-05-19 05:15:14 +0200

    all

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Executes each time when the 'models' package is imported"""
-from models.engine.file_storage import FileStorage
+from models.file_storage import FileStorage
 
 
 storage = FileStorage()
