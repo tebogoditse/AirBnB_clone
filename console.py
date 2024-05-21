33486990a327e9f35d104628ae7fe7bed8a61852
tebogoditse <tebogoditse2@gmail.com>
2024-05-21 18:35:23 +0200

    console

--- console.py
+++ console.py
@@ -6,17 +6,19 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter methods"""
+
+    prompt = "(hbnb)"
+
     def do_quit(self, arg):
-        return True
-    
-    def do_EOF(self, line):
-        print("")
         return True
 
     def help_quit(self, arg):
         message = "Quit command to exit the program"
         print(f"{message}")
-
+    
+    def do_EOF(self, arg):
+        print()
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
